a9b669f30ab122200f7f60a6a4dd8e1b1899f655
Alasdair Monk <alasdair.monk@gmail.com>
2023-10-08 10:16:02 +0100

    Update ogpk.rb

--- ogpk.rb
+++ ogpk.rb
@@ -1,9 +1,9 @@
 class Ogpk < Formula
   desc "CLI tool to fetch OpenGraph data from a URL"
   homepage "https://github.com/almonk/ogpk"
-  url "https://github.com/almonk/ogpk/releases/download/0.1.0/ogpk-0.1.0-darwin-amd64"
-  sha256 "6d81c9928f845c3fc257c07600303c8ed5a4ba80d9a90c7c3d1de5e672674281"
-  version "0.1.0"
+  url "https://github.com/almonk/ogpk/releases/download/0.1.1/ogpk-0.1.1-darwin-amd64"
+  sha256 "4a29bd31ec31661d1319ef66a2ada12cb4491ace97492364b791398bf03d0bd3"
+  version "0.1.1"
 
   def install
     bin.install "ogpk-#{version}-darwin-amd64" => "ogpk"

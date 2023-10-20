dec8b1580c1b3080c9c9f93e0ee2ba76d0643f47
Alasdair Monk <alasdair.monk@gmail.com>
2023-10-20 20:49:01 +0100

    Update ogpk.rb

--- ogpk.rb
+++ ogpk.rb
@@ -1,11 +1,17 @@
 class Ogpk < Formula
   desc "CLI tool to fetch OpenGraph data from a URL"
   homepage "https://github.com/almonk/ogpk"
-  url "https://github.com/almonk/ogpk/releases/download/0.1.3/ogpk-0.1.3-darwin-amd64"
-  sha256 "85952dca516db6581510e830da69e54e69fcb3ebb15ec776e2e0054597e737c4"
   version "0.1.3"
+  if Hardware::CPU.arm?
+    url "https://github.com/almonk/ogpk/releases/download/0.1.3/ogpk-0.1.3-darwin-arm64"
+    sha256 "e5006c7fcd6f7337cb5a77ae2ba4fb531728e9fff8f882f36139fbaed92baa63"
+Processing architecture arm64 ...
+  else
+    url "https://github.com/almonk/ogpk/releases/download/0.1.3/ogpk-0.1.3-darwin-amd64"
+    sha256 "296ec2d80a2de5d711e438827e27d76ec1b5ba31690af39f38668921f9ec1a4e"
+  end
 
   def install
-    bin.install "ogpk-#{version}-darwin-amd64" => "ogpk"
+    bin.install "ogpk-0.1.3-darwin-"+ (Hardware::CPU.arm? ? "arm64" : "amd64") => "ogpk"
   end
 end

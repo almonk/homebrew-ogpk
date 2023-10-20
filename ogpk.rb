2a3440dcb734b44beea6603e0da5eb48aa5acabf
Alasdair Monk <alasdair.monk@gmail.com>
2023-10-20 20:55:31 +0100

    Update ogpk.rb

--- ogpk.rb
+++ ogpk.rb
@@ -2,14 +2,20 @@ class Ogpk < Formula
   desc "CLI tool to fetch OpenGraph data from a URL"
   homepage "https://github.com/almonk/ogpk"
   version "0.1.3"
-  if Hardware::CPU.arm?
-    url "https://github.com/almonk/ogpk/releases/download/0.1.3/ogpk-0.1.3-darwin-arm64"
+  if Hardware::CPU.arm? && "amd64" == "arm64"
+    url "https://github.com/almonk/ogpk/releases/download/0.1.3/ogpk-0.1.3-darwin-amd64"
     sha256 "e5006c7fcd6f7337cb5a77ae2ba4fb531728e9fff8f882f36139fbaed92baa63"
-  else
+  elsif "amd64" == "amd64"
     url "https://github.com/almonk/ogpk/releases/download/0.1.3/ogpk-0.1.3-darwin-amd64"
     sha256 "e5006c7fcd6f7337cb5a77ae2ba4fb531728e9fff8f882f36139fbaed92baa63"
   end
-Processing architecture arm64 ...
+  if Hardware::CPU.arm? && "arm64" == "arm64"
+    url "https://github.com/almonk/ogpk/releases/download/0.1.3/ogpk-0.1.3-darwin-arm64"
+    sha256 "296ec2d80a2de5d711e438827e27d76ec1b5ba31690af39f38668921f9ec1a4e"
+  elsif "arm64" == "amd64"
+    url "https://github.com/almonk/ogpk/releases/download/0.1.3/ogpk-0.1.3-darwin-arm64"
+    sha256 "296ec2d80a2de5d711e438827e27d76ec1b5ba31690af39f38668921f9ec1a4e"
+  end
   def install
     if Hardware::CPU.arm?
       bin.install "ogpk-0.1.3-darwin-arm64" => "ogpk"

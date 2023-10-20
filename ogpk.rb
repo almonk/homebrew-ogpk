2550a400f0f2e34d8b45f7b0c520e482b401bab0
Alasdair Monk <alasdair.monk@gmail.com>
2023-10-20 20:50:30 +0100

    Update ogpk.rb

--- ogpk.rb
+++ ogpk.rb
@@ -5,13 +5,16 @@ class Ogpk < Formula
   if Hardware::CPU.arm?
     url "https://github.com/almonk/ogpk/releases/download/0.1.3/ogpk-0.1.3-darwin-arm64"
     sha256 "e5006c7fcd6f7337cb5a77ae2ba4fb531728e9fff8f882f36139fbaed92baa63"
-Processing architecture arm64 ...
   else
     url "https://github.com/almonk/ogpk/releases/download/0.1.3/ogpk-0.1.3-darwin-amd64"
-    sha256 "296ec2d80a2de5d711e438827e27d76ec1b5ba31690af39f38668921f9ec1a4e"
+    sha256 "e5006c7fcd6f7337cb5a77ae2ba4fb531728e9fff8f882f36139fbaed92baa63"
   end
-
+Processing architecture arm64 ...
   def install
-    bin.install "ogpk-0.1.3-darwin-"+ (Hardware::CPU.arm? ? "arm64" : "amd64") => "ogpk"
+    if Hardware::CPU.arm?
+      bin.install "ogpk-0.1.3-darwin-arm64" => "ogpk"
+    else
+      bin.install "ogpk-0.1.3-darwin-amd64" => "ogpk"
+    end
   end
 end
